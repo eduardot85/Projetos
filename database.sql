02ab856aa7343a81f520db3d3b1e7bbd2a33754c
eduardot85 <eduardot85@yahoo.com.br>
2016-11-21 10:25:13 -0200

    alterações
    Adicao do sql da tabela Pedidos

--- database.sql
+++ database.sql
@@ -18,3 +18,21 @@ CREATE TABLE `produtos` (
   PRIMARY KEY  (`id`)
 );
 
+CREATE TABLE `pedidos` (
+    `id_pedido` INT NOT NULL AUTO_INCREMENT,
+    `id_cliente` INT NOT NULL,
+    `id_produto` INT NOT NULL,
+
+    PRIMARY KEY(id_pedido),
+    INDEX (id_produto),
+    INDEX (id_cliente),
+
+    FOREIGN KEY (id_produto)
+      REFERENCES produtos(id)
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+
+    FOREIGN KEY (id_cliente)
+      REFERENCES clientes(id)
+)
+
+
